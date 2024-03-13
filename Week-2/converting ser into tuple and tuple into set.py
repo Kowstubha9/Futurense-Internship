f3b3610147fd0b882f331c37c5dddd22d40bb93b
Kowstubha9 <134498193+Kowstubha9@users.noreply.github.com>
2024-03-13 09:31:01 +0530

    Create converting ser into tuple and tuple into set.py

---
+++ Week-2/converting ser into tuple and tuple into set.py
@@ -0,0 +1,9 @@
+#python program to convert set into tuple 
+s={1,2,3,4}
+print("Given set:",s)
+t=tuple(s)
+print("After converting given set into tuple:",t)
+#python program to convert tuple into set
+t1=(1,1,2,3,4)
+print("Given tuple:",t1)
+print("After converting given tuple into set:",set(t1))
